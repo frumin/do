419109bc5733a2da8bbc4fe8794705fd35dfde7b
Tom Zaworowski <tom@acidbits.com>
2025-03-08 22:50:36 -0800

    Add support for removing multiple tasks at once 🗑️

--- Sources/todo/Commands/RemoveCommand.swift
+++ Sources/todo/Commands/RemoveCommand.swift
@@ -4,21 +4,51 @@ import Foundation
 struct RemoveCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "remove",
-        abstract: "Remove a task from your list 🗑️"
+        abstract: "Remove tasks from your list 🗑️"
     )
     
-    @Argument(help: "Which task would you like to remove? (enter its number)")
-    var number: Int
+    @Argument(parsing: .remaining, help: "Which tasks would you like to remove? (enter their numbers, space-separated)")
+    private var numberStrings: [String]
+    
+    var numbers: [Int] {
+        numberStrings.compactMap { Int($0) }
+    }
     
     func run() throws {
-        var todos = try Todo.storage.readTodos()
-        guard number > 0 && number <= todos.count else {
-            throw ValidationError("Oops! That task number doesn't exist. Try 'todo list' to see your tasks and their numbers 🔍")
+        guard !numbers.isEmpty else {
+            throw ValidationError("Please specify which tasks to remove (enter their numbers)")
         }
-        let todo = todos[number - 1]
-        todos.remove(at: number - 1)
-        try Todo.storage.writeTodos(todos)
-        print("🗑️ Task removed:")
-        print(todo.format(index: number))
+        
+        // Check if any numbers couldn't be parsed
+        if numbers.count != numberStrings.count {
+            throw ValidationError("Please enter valid task numbers")
+        }
+        
+        let todos = try Todo.storage.readTodos()
+        
+        // Validate all numbers first
+        for number in numbers {
+            guard number > 0 && number <= todos.count else {
+                throw ValidationError("Oops! Task #\(number) doesn't exist. Try 'todo list' to see your tasks and their numbers 🔍")
+            }
+        }
+        
+        // Sort in reverse order to handle indices correctly
+        let sortedNumbers = numbers.sorted(by: >)
+        
+        // Remove each task
+        if sortedNumbers.count == 1 {
+            let todo = todos[sortedNumbers[0] - 1]
+            try Todo.storage.archiveTodo(todo, reason: .deleted)
+            print("🗑️ Task removed:")
+            print(todo.format(index: sortedNumbers[0]))
+        } else {
+            print("🗑️ Removed \(sortedNumbers.count) tasks:")
+            for number in sortedNumbers {
+                let todo = todos[number - 1]
+                try Todo.storage.archiveTodo(todo, reason: .deleted)
+                print(todo.format(index: number))
+            }
+        }
     }
 } 
